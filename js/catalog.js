25c2ce55e657b7a4236743c62d41a0a945944da9
MIFIgor <makarov102@yandex.ru>
2020-10-12 20:30:40 +0700

    Доступность и формы

--- js/catalog.js
+++ js/catalog.js
@@ -12,11 +12,11 @@ for (let elem of elems) {
     showBlock.style.top='0';
     showBlock.style.cursor='pointer'
     showBlock.style.zIndex='2';
-    showBlock.style.display='block';
+    showBlock.classList.remove('visually-hidden');
   });
 
   elem.addEventListener('mouseout', () => {
-    showBlock.style.display='none';
+    showBlock.classList.add('visually-hidden');
   });
 }
 
